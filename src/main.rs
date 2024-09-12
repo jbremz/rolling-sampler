4053f5f56b3e39f0ae60c4c521edf350de7f70de
seandavies555 <sean@fourieraudio.com>
2024-09-12 18:35:47 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Mutex;
-use eframe::{run_native, App, CreationContext};
-use egui::{CentralPanel, Vec2b, RichText};
 use std::error::Error;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+
+use chrono::Utc;
 use cpal::traits::{DeviceTrait, HostTrait};
 use cpal::{Device, SampleFormat, StreamConfig};
-use hound::{WavWriter, WavSpec, SampleFormat as HoundSampleFormat};
-use rfd::FileDialog;
-use chrono::Utc;
-use egui_plot::{Line, Plot, PlotUi, PlotPoints, Corner, CoordinatesFormatter};
 use dirs::home_dir;
-
+use eframe::{run_native, App, CreationContext};
+use egui::{CentralPanel, RichText, Vec2b};
+use egui_plot::{CoordinatesFormatter, Corner, Line, Plot, PlotPoints, PlotUi};
+use hound::{SampleFormat as HoundSampleFormat, WavSpec, WavWriter};
+use rfd::FileDialog;
 
 struct Recorder {
     is_grabbing: Arc<AtomicBool>,
@@ -20,8 +19,8 @@ struct Recorder {
     config: StreamConfig,
     buffer_size: Arc<Mutex<usize>>,
     save_path: Option<String>,
-    devices: Vec<Device>,                // Store available input devices
-    current_device_index: usize,          // Store the index of the selected device
+    devices: Vec<Device>,        // Store available input devices
+    current_device_index: usize, // Store the index of the selected device
 }
 struct CircularBuffer {
     circular_buffer: Vec<f32>,
@@ -40,23 +39,25 @@ fn get_file_safe_timestamp() -> String {
     now.format("%Y-%m-%d_%H-%M-%S").to_string()
 }
 
-
 impl Recorder {
     fn new(initial_buffer_size: usize) -> Self {
         let host = cpal::default_host();
-        let devices: Vec<Device> = host.input_devices().unwrap().collect();  // Fetch available devices
+        let devices: Vec<Device> = host.input_devices().unwrap().collect(); // Fetch available devices
 
         let current_device_index = 0; // Set default to the first device
         let input_device = devices[current_device_index].clone();
-        let config = input_device.default_input_config().expect("Failed to get default input config");
+        let config =
+            input_device.default_input_config().expect("Failed to get default input config");
         let config: StreamConfig = config.into();
         let initial_buffer_size = initial_buffer_size * config.sample_rate.0 as usize;
 
         // Resolve the Desktop path and convert it to a String
-        let save_path: Option<String> = home_dir().map(|mut path| {
-            path.push("Desktop");
-            path.to_str().map(|s| s.to_owned()) // Convert to String
-        }).flatten(); // Flatten the Option<Option<String>> to Option<String>
+        let save_path: Option<String> = home_dir()
+            .map(|mut path| {
+                path.push("Desktop");
+                path.to_str().map(|s| s.to_owned()) // Convert to String
+            })
+            .flatten(); // Flatten the Option<Option<String>> to Option<String>
 
         let mut recorder = Recorder {
             is_grabbing: Arc::new(AtomicBool::new(false)),
@@ -74,12 +75,12 @@ impl Recorder {
     }
 
     fn start_recording(&mut self) {
-
         // Get the currently selected device
         let input_device = self.devices[self.current_device_index].clone();
 
         // Fetch the latest configuration
-        let config = input_device.default_input_config().expect("Failed to get default input config");
+        let config =
+            input_device.default_input_config().expect("Failed to get default input config");
         let config: StreamConfig = config.into();
         self.config = config;
         let sample_format = input_device.default_input_config().unwrap().sample_format();
@@ -88,17 +89,15 @@ impl Recorder {
         let sample_buffer = Arc::clone(&self.sample_buffer);
 
         let stream = match sample_format {
-            SampleFormat::F32 => {
-                input_device.build_input_stream(
-                    &self.config,
-                    move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                        let mut buffer = sample_buffer.lock().unwrap();
-                        buffer.add_samples(data);
-                    },
-                    err_fn,
-                    None,
-                )
-            }
+            SampleFormat::F32 => input_device.build_input_stream(
+                &self.config,
+                move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                    let mut buffer = sample_buffer.lock().unwrap();
+                    buffer.add_samples(data);
+                },
+                err_fn,
+                None,
+            ),
             _ => panic!("Unsupported sample format"),
         }
         .unwrap();
@@ -115,18 +114,18 @@ impl Recorder {
     fn grab_recording(&mut self) {
         self.is_grabbing.store(false, Ordering::SeqCst);
         if let Some(stream) = self.stream.take() {
-            drop(stream);  // This drops the stream and stops recording
+            drop(stream); // This drops the stream and stops recording
         }
-    
+
         let buffer = self.sample_buffer.lock().unwrap();
-    
+
         // Determine the number of samples in the buffer
         let num_samples = buffer.current_size;
         let num_channels = self.config.channels as usize;
-    
+
         println!("Recorded shape: ({}, {})", num_samples / num_channels, num_channels);
         println!("Sample rate: {}", self.config.sample_rate.0);
-    
+
         // Prepare WAV writer specifications based on the buffer content
         let spec = WavSpec {
             channels: self.config.channels,
@@ -134,26 +133,26 @@ impl Recorder {
             bits_per_sample: 32, // Assuming f32 for this example
             sample_format: HoundSampleFormat::Float,
         };
-    
-        let filepath = format!("{}/{}.wav", self.save_path.as_ref().unwrap(), get_file_safe_timestamp());
+
+        let filepath =
+            format!("{}/{}.wav", self.save_path.as_ref().unwrap(), get_file_safe_timestamp());
         let mut writer = WavWriter::create(filepath, spec).expect("Failed to create WAV writer");
-    
+
         // Save buffer
         for &sample in buffer.static_buffer.iter().take(buffer.current_size) {
             writer.write_sample(sample).unwrap();
         }
-        
+
         writer.finalize().expect("Failed to finalize WAV writer");
-    
+
         println!("Recording saved!");
-    
+
         // Replace the buffer with a new one rather than clearing the old one
-        drop(buffer);  // Unlock the mutex before replacing the buffer
+        drop(buffer); // Unlock the mutex before replacing the buffer
 
         // Restart recording with a fresh stream
         self.start_recording();
     }
-    
 
     fn update_buffer_size(&mut self, new_size: usize) {
         {
@@ -162,11 +161,10 @@ impl Recorder {
             *buffer_size = new_size;
             // The lock (buffer_size) will be dropped at the end of this scope
         }
-    
+
         // Reset the buffer after the lock on buffer_size has been released
         self.reset_buffer();
     }
-    
 
     fn open_file_dialog(&mut self) {
         if let Some(path) = FileDialog::new().pick_folder() {
@@ -247,36 +245,41 @@ impl CircularBuffer {
     }
 }
 
-
 impl App for Recorder {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
-
         // Repaint the UI to update the plot
         ctx.request_repaint();
 
         CentralPanel::default().show(ctx, |ui| {
-
             ui.add_space(10.0); // Add some space at the top
-            
-            ui.vertical_centered(|ui| {
 
+            ui.vertical_centered(|ui| {
                 // ui.add_space(10.0); // Add some space at the top
                 let panel_width = ui.available_width();
 
                 // Center the contents inside the horizontal layout
                 ui.vertical_centered(|ui| {
-
-                    // Device selection dropdown - can't centre this because it isn't an atomic widget 🤷
-                    ui.horizontal( |ui| {
+                    // Device selection dropdown - can't centre this because it isn't an atomic
+                    // widget 🤷
+                    ui.horizontal(|ui| {
                         ui.label("Input Device:");
                         let current_device_index = self.current_device_index; // Store the current device index for later comparison
-                        egui::ComboBox::from_id_source("Device")  // Using an ID instead of a label
-                        .selected_text(self.devices[self.current_device_index].name().unwrap_or_default().clone())
-                        .show_ui(ui, |ui| {
-                            for (idx, device) in self.devices.iter().enumerate() {
-                                ui.selectable_value(&mut self.current_device_index, idx, device.name().unwrap_or_default());
-                            }
-                        });
+                        egui::ComboBox::from_id_source("Device") // Using an ID instead of a label
+                            .selected_text(
+                                self.devices[self.current_device_index]
+                                    .name()
+                                    .unwrap_or_default()
+                                    .clone(),
+                            )
+                            .show_ui(ui, |ui| {
+                                for (idx, device) in self.devices.iter().enumerate() {
+                                    ui.selectable_value(
+                                        &mut self.current_device_index,
+                                        idx,
+                                        device.name().unwrap_or_default(),
+                                    );
+                                }
+                            });
                         // Check if the selected device has changed
                         if current_device_index != self.current_device_index {
                             // Stop current recording
@@ -285,7 +288,10 @@ impl App for Recorder {
                             }
                             // Update config for new device
                             let new_device = &self.devices[self.current_device_index];
-                            self.config = new_device.default_input_config().expect("Failed to get default input config").into();
+                            self.config = new_device
+                                .default_input_config()
+                                .expect("Failed to get default input config")
+                                .into();
                             // Start recording with new device
                             self.start_recording();
                         }
@@ -303,7 +309,7 @@ impl App for Recorder {
                             .iter()
                             .enumerate()
                             .filter(|(i, _)| i % downsample_factor == 0) // Pick every Nth sample
-                            .map(|(i, &sample)| [i as f64, sample as f64])  // Create [x, y] pairs
+                            .map(|(i, &sample)| [i as f64, sample as f64]) // Create [x, y] pairs
                             .collect();
 
                         let plot_points = PlotPoints::new(points);
@@ -311,13 +317,15 @@ impl App for Recorder {
                         // Create a line from the points
                         let line = Line::new(plot_points);
 
-                        // this didn't do what I wanted it to do but I think it's something along these lines
-                        let no_coordinates_formatter = CoordinatesFormatter::new(|_, _| String::new());
+                        // this didn't do what I wanted it to do but I think it's something along
+                        // these lines
+                        let no_coordinates_formatter =
+                            CoordinatesFormatter::new(|_, _| String::new());
 
                         // Display the plot
                         Plot::new("Rolling Waveform Plot")
-                            .view_aspect(4.0)  // Adjust aspect ratio if necessary
-                            .auto_bounds(Vec2b::new(true, false))  // Disable auto bounds for y-axis, keep x-axis auto-bounds
+                            .view_aspect(4.0) // Adjust aspect ratio if necessary
+                            .auto_bounds(Vec2b::new(true, false)) // Disable auto bounds for y-axis, keep x-axis auto-bounds
                             .show_axes(false)
                             .show_grid(false)
                             .show_background(false)
@@ -325,13 +333,16 @@ impl App for Recorder {
                             .allow_drag(false)
                             .allow_scroll(false)
                             .sharp_grid_lines(true)
-                            .coordinates_formatter(Corner::LeftBottom, no_coordinates_formatter)  // Disable coordinates display
+                            .coordinates_formatter(Corner::LeftBottom, no_coordinates_formatter) // Disable coordinates display
                             .show(ui, |plot_ui: &mut PlotUi| {
                                 plot_ui.line(line);
                             });
                     }
 
-                    ui.label(RichText::new("Choose how much past audio to include in the recording:").italics());
+                    ui.label(
+                        RichText::new("Choose how much past audio to include in the recording:")
+                            .italics(),
+                    );
 
                     // Slider to control buffer size
                     let mut buffer_size = *self.buffer_size.lock().unwrap();
@@ -344,13 +355,17 @@ impl App for Recorder {
                     let max_buffer_seconds = 60.0; // Maximum 60 seconds for the slider
                     let mut new_buffer_size_seconds = buffer_size_seconds;
 
-                    ui.horizontal( |ui| {
-                        ui.label("Buffer Size (s):");  // Text label before the slider
-                        let response = ui.add(egui::Slider::new(&mut new_buffer_size_seconds, 1.0..=max_buffer_seconds));
+                    ui.horizontal(|ui| {
+                        ui.label("Buffer Size (s):"); // Text label before the slider
+                        let response = ui.add(egui::Slider::new(
+                            &mut new_buffer_size_seconds,
+                            1.0..=max_buffer_seconds,
+                        ));
                         // .text("Buffer Size (s)"));
-    
-                        let new_buffer_size = (new_buffer_size_seconds * self.config.sample_rate.0 as f32) as usize;
-    
+
+                        let new_buffer_size =
+                            (new_buffer_size_seconds * self.config.sample_rate.0 as f32) as usize;
+
                         if response.drag_stopped() && new_buffer_size != buffer_size {
                             buffer_size = new_buffer_size;
                             self.update_buffer_size(buffer_size);
@@ -378,7 +393,8 @@ impl App for Recorder {
                         "Start Grab"
                     };
 
-                    if ui.add_sized([100.0, 40.0], egui::Button::new(record_button_text)).clicked() {
+                    if ui.add_sized([100.0, 40.0], egui::Button::new(record_button_text)).clicked()
+                    {
                         if self.is_grabbing.load(Ordering::SeqCst) {
                             println!("Stop button clicked");
                             self.grab_recording();
@@ -395,8 +411,6 @@ impl App for Recorder {
     }
 }
 
-
-
 fn err_fn(err: cpal::StreamError) {
     eprintln!("An error occurred on the input stream: {}", err);
 }
@@ -404,13 +418,13 @@ fn err_fn(err: cpal::StreamError) {
 fn main() -> Result<(), Box<dyn Error>> {
     let app_name = "Rolling Sampler";
     let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([800.0, 430.0]), // Set your desired width and height
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 430.0]), /* Set your desired width and height */
         ..Default::default()
     };
-    let app_creator = move |_cc: &CreationContext| -> Result<Box<dyn App>, Box<dyn Error + Send + Sync>> {
-        Ok(Box::new(Recorder::new(5))) // Initialize with a buffer size of 44100 samples
-    };
+    let app_creator =
+        move |_cc: &CreationContext| -> Result<Box<dyn App>, Box<dyn Error + Send + Sync>> {
+            Ok(Box::new(Recorder::new(5))) // Initialize with a buffer size of 44100 samples
+        };
     run_native(app_name, native_options, Box::new(app_creator))?;
     Ok(())
 }
